47092593d10d8a8e72b1531ef33148f5d876741f
RARKA228 <ksushachistukh@gmail.com>
2024-12-24 18:30:35 +0700

    main file commit 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, Jenkins and Hello and of study !
-    Lets selebrate the New Year and Christmas" << std::endl;
+    std::cout << "Hello, Jenkins and Hello ending of study! "
+                 "Let's celebrate the New Year and Christmas!" << std::endl;
     return 0;
 }
